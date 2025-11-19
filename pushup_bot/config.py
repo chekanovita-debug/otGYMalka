5716a5391444b96376c818852df2035da9c22378
chekanovita-debug <chekanovita@gmail.com>
2025-11-19 23:08:39 +0300

    Delete pushup_bot directory

--- pushup_bot/config.py
+++
@@ -1,5 +0,0 @@
-BOT_TOKEN = "8402939058:AAG6WOwEscMSo0QCH9WJhkJvUnZhCUuIN0E"
-CHALLENGE_START = "2025-11-17"
-CHALLENGE_DAYS = 100               # длительность в днях
-# Можно добавить DAILY_TARGET, если делаете таргет не 100
-DAILY_TARGET = 100
